c01235cc6e4b5f95ffa5ea1550fe918c81f40631
Jonathan Duran <jduran9987@gmail.com>
2022-06-20 16:32:40 -0400

    added conditions for docker image tag

---
+++ dockerTagging.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export IMAGE_TAG=latest
+
+if [ $CODEBUILD_SOURCE_VERSION != "main" ] && ([ $CODEBUILD_WEBHOOK_BASE_REF != "refs/heads/main"] || [ $CODEBUILD_WEBHOOK_BASE_REF == ""])
+do
+    export DATE_SUFFIX=$(date +%s)
+    export IMAGE_TAG=$CODEBUILD_RESOLVED_SOURCE_VERSION-$DATE_SUFFIX
+fi
