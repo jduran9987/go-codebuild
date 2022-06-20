804bb43cf866431a75bbd7cbff35facb64362c38
Jonathan Duran <jduran9987@gmail.com>
2022-06-20 19:04:15 -0400

    removed source version

--- dockerTagging.sh
+++ dockerTagging.sh
@@ -2,7 +2,7 @@
 
 export IMAGE_TAG=latest
 
-if [ $CODEBUILD_SOURCE_VERSION != "main" ] && ([ $CODEBUILD_WEBHOOK_BASE_REF != "refs/heads/main"] || [ $CODEBUILD_WEBHOOK_BASE_REF == ""])
+if [ $CODEBUILD_SOURCE_VERSION != "main" ] && ([ $CODEBUILD_WEBHOOK_BASE_REF != "refs/heads/main" ] || [ $CODEBUILD_WEBHOOK_BASE_REF == "" ])
 then
     export DATE_SUFFIX=$(date +%s)
     export IMAGE_TAG=$CODEBUILD_RESOLVED_SOURCE_VERSION-$DATE_SUFFIX
